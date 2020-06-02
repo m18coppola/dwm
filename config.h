e3e5a128fb27fdcbf269fa3342d426d18a10f09d
Michael Coppola <m18coppola@gmail.com>
2020-06-01 22:45:33 -0400

    attempted to fix backlight up/down

--- config.h
+++ config.h
@@ -59,8 +59,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
-static const char *brightness_up[] = {"brightnessctl set 10%+"};
-static const char *brightness_down[] = {"brightnessctl set 10%-"};
+static const char *brightness_up[] = {"brightnessctl", "set", "10%+"};
+static const char *brightness_down[] = {"brightnessctl", "set", "10%-"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
